cf4538d6c799ed84be564c4b07c647116c81250b
Iven Day <ivenvd@gmail.com>
2009-01-18 11:51:58 +0800

    Use FindGTK2 module to find GTK+.

--- src/interface.c
+++ src/interface.c
@@ -3,7 +3,13 @@
 GtkWidget *create_main_window (void) {
     GtkWidget *window;
     
+    //Setup main window
     window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title (GTK_WINDOW (window), "Cugb Freer");
+    gtk_widget_set_size_request (window, 600, 500);
+    gtk_window_set_resizable (GTK_WINDOW (window), TRUE);
+    gtk_container_set_border_width (GTK_CONTAINER (window), 10);
+    gtk_window_set_position (GTK_WINDOW (window), GTK_WIN_POS_CENTER);
     g_signal_connect (G_OBJECT (window), "delete-event", G_CALLBACK (gtk_main_quit), NULL);
     return window;
 }
